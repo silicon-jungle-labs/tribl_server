ac508012c2aa8a56251c8d8bf630727868672b59
codeNgamer <ekeneoduah@live.com>
2016-08-13 18:51:12 -0500

    update prod mongo url

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = environment => {
       mongoUrl: 'mongodb://localhost:27017/tribl',
     },
     production: {
-      mongoUrl: 'mongodb://tribl:tiblAdmin@jello.modulusmongo.net:27017/oneNi4my',
+      mongoUrl: 'mongodb://tribl:triblAdmin@jello.modulusmongo.net:27017/oneNi4my',
     }
   };
   return options[environment];
